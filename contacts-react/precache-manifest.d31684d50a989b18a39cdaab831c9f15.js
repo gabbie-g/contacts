1b9028e7f359721aead5f6093aff233b55f8ede5
gabbie-g <88021584+gabbie-g@users.noreply.github.com>
2021-08-20 14:24:40 +0200

    Add files via upload

---
+++ contacts-react/precache-manifest.d31684d50a989b18a39cdaab831c9f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeee7abf798c2d5c1e6fdf95eae0c220",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4bcff96510a8b7a136d",
+    "url": "./static/css/main.1dc38e42.chunk.css"
+  },
+  {
+    "revision": "5bbd4ee25ab464272c45",
+    "url": "./static/js/2.8795ba2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8795ba2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bcff96510a8b7a136d",
+    "url": "./static/js/main.b2524da2.chunk.js"
+  },
+  {
+    "revision": "c00bd2c07f694aa343c6",
+    "url": "./static/js/runtime-main.14f48e75.js"
+  }
+]);
